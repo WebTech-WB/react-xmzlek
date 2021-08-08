11f73021f8e8522a7954a154de652a9ef023dbcd
WebTech-WB <webtechwb@yahoo.com>
2021-08-08 16:02:08 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './style.css';
+
+export default function App() {
+  return (
+    <>
+      <div class="top-navbar">
+        <div id="ham-btn">☰</div>
+
+        <a href="https://sloppydeals.com/" title="Logo Link" target="_self">
+          <h2>Sloppydeals</h2>
+        </a>
+
+        <a href="https://sloppydeals.com/" title="Logo Link" target="_self">
+          <i class="fa fa-shopping-cart" />
+        </a>
+      </div>
+    </>
+  );
+}
